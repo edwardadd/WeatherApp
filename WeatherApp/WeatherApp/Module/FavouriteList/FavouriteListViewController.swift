80253b2a3d78c9ef861fe4c96f2d7a63e7946801
Edward Addley <me@edwadaddley.co.uk>
2019-10-23 11:14:09 +0100

    Add controls to favourites list view controller

--- WeatherApp/WeatherApp/Module/FavouriteList/FavouriteListViewController.swift
+++ WeatherApp/WeatherApp/Module/FavouriteList/FavouriteListViewController.swift
@@ -9,4 +9,20 @@
 import UIKit
 
 class FavouriteListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var emptyAddButton: UIButton!
+
+    override func viewDidLoad() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self,
+                                                            action: #selector(addButtonTapped(_:)))
+        emptyAddButton.addTarget(self, action: #selector(addButtonTapped(_:)), for: .touchUpInside)
+    }
+}
+
+extension FavouriteListViewController {
+    @objc
+    func addButtonTapped(_ sender: UIBarButtonItem) {
+
+    }
 }
